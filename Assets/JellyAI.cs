4a1fc5b7744b00361dd4915aeb59427ab6248b2b
alexandraCoding <51174166+alexandraCoding@users.noreply.github.com>
2020-07-04 03:57:01 +0000

    Initial commit

---
+++ Assets/JellyAI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+
+public class JellyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float speed = 0.5f;
+    public Rigidbody rb;
+    public float jumpforce = 6f;
+    private Vector3 jmp;
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        jmp = transform.up * jumpforce;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+        transform.LookAt(target);
+        if (transform.position.y > target.position.y + 1.5f)
+        {
+
+            Jump();
+        }
+       /* if (Vector3.Distance(transform.position, target.position) < 2f)
+            SceneManager.LoadScene(SceneManager.GetActiveScene() );
+       */
+    }
+        bool isGrounded() {
+            Collider[] cc = Physics.OverlapSphere(new Vector3(transform.position.x, transform.position.y - 1f, transform.position.z), 0.7f);
+            int j = 0;
+            for (int i = 0; i < cc.Length; i++) {
+
+                if (cc[i].gameObject != gameObject) {
+                    j++;
+
+                }
+            }
+            return j > 0;
+        }
+        void Jump() {
+            if (isGrounded()) {
+                rb.AddForce(jmp, ForceMode.Impulse); }
+        }
+      /*  void OnCollisionEnter( Collision other) {
+            if(other.gameObject.GetComponent<Shooting>().name)
+            SceneManager.LoadScene(SceneManager.GetActiveScene());*/
+        }
+    
+    
+
---
+++ Assets/Shooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 100f)&&(hit.collider.gameObject!= gameObject))
+                 {  if(hit.collider.gameObject.GetComponent<JellyAI>())
+                Destroy(hit.collider.gameObject);
+        }
+
+        }
+       
+        
+    }
+}
